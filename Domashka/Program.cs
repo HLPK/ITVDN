d8d19e609275c68bb202a33d5c9599ab3da2d97e
Галина Павлюк <gp0407@gmail.com>
2021-04-23 16:43:38 +0300

    Добавьте файлы проекта.

---
+++ Domashka/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domashka
+{
+    class Program
+    {
+        static int count=0;
+        static void Procedure()
+        {
+            Console.WriteLine($"Current thread id: {Thread.CurrentThread.ManagedThreadId}");
+            for (int i = 0; i < 10; i++)
+            {
+                count++;
+                Console.Write(count.ToString()+" ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main thread id: {Thread.CurrentThread.GetHashCode()}");
+            Procedure();
+            
+            Thread thread2 = new Thread(Procedure);
+            thread2.Start();
+            thread2.Join();
+
+            Thread thread3 = new Thread(Procedure);
+            thread3.Start();
+            thread3.Join();
+
+            Console.ReadKey();
+        }
+    }
+}
